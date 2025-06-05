173d0148dfb06de68ddf28582b0a0f7a075f5699
JesusCarmona <ryzen3000@outlook.es>
2025-06-04 22:25:00 -0600

    prueba netlify

--- src/routes/contacto/+page.svelte
+++ src/routes/contacto/+page.svelte
@@ -1,5 +1,5 @@
 <main>
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" data-netlify="true" action="/">
         <input type="hidden" name="contact" value="contact">
         <div class="relativa isolate px-6 py-8 lg:px-1 space-y-1 mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 lg:-mt-10 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-1">
             <h1 class="text-base/7 text-gray-900 text-xl ">Revoluciona tu clínica dental Ahora <span class="font-bold">¡Gratis!</span></h1>
